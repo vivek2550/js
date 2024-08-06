3afdc3a3eeec989b358831919e3bb7332d17efeb
vivek2550 <vp760480@gmail.com>
2024-08-06 22:43:45 +0530

    my first program

---
+++ pract1.js
@@ -0,0 +1,16 @@
+const pen={
+    color:"black",
+    brand:"Cello",
+    price:100,
+};
+console.log(typeof(pen));
+
+let profile={
+    name:"vivek",
+    age:20,
+    address:"kolkata",
+    domain:"student",
+    };
+     profile["name"]="rahul";
+    console.log(profile["name"]);
+    console.log(profile);
