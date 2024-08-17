9c581313d8a9c3ed2992693b3ad9ddc0c5b22208
vivek2550 <vp760480@gmail.com>
2024-08-17 21:24:26 +0530

    methods of js

---
+++ meth.js
@@ -0,0 +1,13 @@
+// use push method
+let arr = [1,2,3,4,5,6,7]
+arr.push(8)
+console.log(arr) 
+// use po meth
+arr.pop()
+console.log(arr)
+//use tostring meth
+console.log(arr.toString())
+
+let arr1 = ["Microsoft", "IBM", "Infosys","Tcs"]
+
+arr1.push("Amazon");
